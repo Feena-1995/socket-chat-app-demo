bfef866ea940dd1dd8f674c804ae885b6d662d75
feena.tian <feena.tian@feenatians-MacBook-Air.local>
2022-02-14 15:17:48 +1100

    feat: 🎸 init commit

---
+++ client/src/App.js
@@ -0,0 +1,48 @@
+import "./App.css";
+import io from "socket.io-client";
+import { useState } from "react";
+import Chat from "./Chat";
+
+const socket = io.connect("http://localhost:3001");
+
+function App() {
+  const [username, setUsername] = useState("");
+  const [roomId, setRoomId] = useState("");
+  const [showChat, setShowChat] = useState(false);
+
+  const joinRoom = () => {
+    if (!!username && !!roomId) {
+      socket.emit("join_room", { roomId, username });
+      setShowChat(true);
+    }
+  };
+
+  return (
+    <div className="App">
+      {!showChat ? (
+        <div className="joinChatContainer">
+          <h3>Join A Chat</h3>
+          <input
+            type="text"
+            placeholder="John..."
+            onChange={(event) => {
+              setUsername(event.target.value);
+            }}
+          />
+          <input
+            type="text"
+            placeholder="Room ID..."
+            onChange={(event) => {
+              setRoomId(event.target.value);
+            }}
+          />
+          <button onClick={joinRoom}>Join A Room</button>
+        </div>
+      ) : (
+        <Chat socket={socket} username={username} roomId={roomId} />
+      )}
+    </div>
+  );
+}
+
+export default App;
